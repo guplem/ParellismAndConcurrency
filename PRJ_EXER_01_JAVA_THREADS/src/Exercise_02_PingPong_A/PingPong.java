ab8f1cacb6ca1e323060c0ef97bb4c1eea8d0dbc
guplem <guplem@gmail.com>
2022-01-20 16:13:43 +0100

    EXERCISE 2 UNSYNCHRONIZED PING PONG

--- PRJ_EXER_01_JAVA_THREADS/src/Exercise_02_PingPong_A/PingPong.java
+++ PRJ_EXER_01_JAVA_THREADS/src/Exercise_02_PingPong_A/PingPong.java
@@ -3,25 +3,46 @@ package Exercise_02_PingPong_A;
 public class PingPong {
     
     public static void main (String [] args) {
+    	
+		System.out.println("EXERCISE 2 UNSYNCHRONIZED PING PONG");
+		System.out.println("-----------------------------------");
+		System.out.println("");
+    	
         Ping ping = new Ping();
         Pong pong = new Pong();
         
-        /* COMPLETE */
+        ping.start();
+        pong.start();
+        
+        try {
+			Thread.sleep(5 * 1000);
+		} catch (InterruptedException e) { }
+        
+        ping.stop();
+        pong.stop();
+        
     }
     
 }
 
-
 class Ping extends Thread {
-    
     public void run () {
         while (true) {
-            /* COMPLETE */
+            System.out.println("PING");
+            try {
+				Thread.sleep(10);
+    		} catch (InterruptedException e) { }
         }
     }
 }
 
 class Pong extends Thread {
-    
-    /* COMPLETE */
+    public void run () {
+        while (true) {
+            System.out.println(" PONG");
+            try {
+				Thread.sleep(10);
+    		} catch (InterruptedException e) { }
+        }
+    }
 }

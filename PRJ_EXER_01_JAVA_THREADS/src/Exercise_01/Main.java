9c557641050cf9c90b23b9f6dd946fedf55bb234
guplem <guplem@gmail.com>
2022-01-20 15:47:07 +0100

    E_1

--- PRJ_EXER_01_JAVA_THREADS/src/Exercise_01/Main.java
+++ PRJ_EXER_01_JAVA_THREADS/src/Exercise_01/Main.java
@@ -2,8 +2,30 @@ package Exercise_01;
 
 public class Main {
 	public static void main (String [] args) throws InterruptedException{
-        RandomizerThread [] threadTable = new RandomizerThread[5];
         
-        /* COMPLETE */
+		System.out.println("EXERCISE 1");
+		System.out.println("----------");
+		System.out.println("");
+		
+		RandomizerThread [] threadTable = new RandomizerThread[5];
+		
+		for(int i = 0; i < threadTable.length; i++) {
+			threadTable[i] = new RandomizerThread();
+			threadTable[i].start();
+		}
+		
+		for(int i = 0; i < threadTable.length; i++) {
+			threadTable[i].join();
+		}
+		
+		int totalSum = 0;
+		for(int i = 0; i < threadTable.length; i++) {
+			totalSum += threadTable[i].GetNumber();
+		}
+       
+
+		System.out.println("");
+		System.out.println("The sum is: " + totalSum);
+        
     }
 }
--- PRJ_EXER_01_JAVA_THREADS/src/Exercise_01/RandomizerThread.java
+++ PRJ_EXER_01_JAVA_THREADS/src/Exercise_01/RandomizerThread.java
@@ -1,8 +1,21 @@
 package Exercise_01;
 
+import java.util.Random;
+
 public class RandomizerThread extends Thread {
     
     private int theNumber=-1000;
     
-    /* COMPLETE */
+    
+    public int GetNumber() {
+    	return theNumber;
+    }
+    
+    @Override
+    public void run() {
+    	theNumber = new Random().nextInt(10);
+    	System.out.println("My number is: " + theNumber);
+    	super.run();
+    }
+    
 }

f111202f84e29b982910848a36ebf9ad1bff0808
Guillem Poy <guplem@gmail.com>
2022-01-27 11:17:17 +0100

    EXERCISE 2 SYNCHRONIZED PING PONG WITH ACTIVE WAITING AND SHARED OBJECT

--- PRJ_EXER_01_JAVA_THREADS/src/Exercise_02_PingPong_BB/PingPong_Sync_B.java
+++ PRJ_EXER_01_JAVA_THREADS/src/Exercise_02_PingPong_BB/PingPong_Sync_B.java
@@ -3,10 +3,16 @@ package Exercise_02_PingPong_BB;
 public class PingPong_Sync_B {
     
     public static void main (String [] args) {
+    	
+		System.out.println("EXERCISE 2 SYNCHRONIZED PING PONG WITH ACTIVE WAITING AND SHARED OBJECT");
+		System.out.println("-----------------------------------------------------------------------");
+		System.out.println("");
+    	
         SharedObject shared = new SharedObject();
         
-        /* COMPLETE */
-        
+        Ping ping = new Ping(shared);
+        Pong pong = new Pong(shared);
+		
         pong.start();
         ping.start();
         
@@ -32,12 +38,15 @@ class Ping extends Thread {
     
     private SharedObject shared;
     
-    /* COMPLETE */
+	public Ping(SharedObject shared) {
+		this.shared = shared;
+	}
     
     public void run () {
         while (true) {
-            while (shared.getWhoCanPrint()!=1) {/* busy wait*/}
-            /* COMPLETE */
+            while (shared.getWhoCanPrint()!=1) {Thread.yield();}
+            System.out.println("PING");
+            shared.setWhoCanPrint(2);
         }
     }
 }
@@ -46,11 +55,15 @@ class Pong extends Thread {
     
     private SharedObject shared;
     
-    /* COMPLETE */
+	public Pong(SharedObject shared) {
+		this.shared = shared;
+	}
     
     public void run () {
         while (true) {
-            /* COMPLETE */
+            while (shared.getWhoCanPrint()!=2) {Thread.yield();}
+            System.out.println("   PONG");
+            shared.setWhoCanPrint(1);
         }
     }
 }
